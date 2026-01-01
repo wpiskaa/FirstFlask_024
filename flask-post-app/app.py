e4be490d8b663b76543c6b8300b2ceb24b7a677e
wpiskaa <hafizzkurniawan19@gmail.com>
2026-01-01 20:07:05 +0700

    Flask POST Parameter Parsing

--- flask-post-app/app.py
+++ flask-post-app/app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+# Mengatur path folder agar Flask tidak bingung mencari file index.html
+template_dir = os.path.abspath(os.path.dirname(__file__))
+template_dir = os.path.join(template_dir, 'templates')
+static_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -9,11 +15,14 @@ def index():
     char_count = 0
 
     if request.method == 'POST':
+        # Mengambil data dari form HTML
         original_text = request.form.get('user_input')
         if original_text:
+            # Logika Python untuk memproses teks
             result = original_text.upper()
             char_count = len(original_text)
 
+    # Menghubungkan variabel Python ke file HTML (index.html)
     return render_template('index.html', 
                            result=result, 
                            original=original_text, 
